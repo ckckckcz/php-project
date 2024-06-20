98334492d8d39a54e54cf8622f959591176b23a3
ckckckcz <rriovld@gmail.com>
2024-06-20 19:10:32 +0700

    feat : setup config

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php 
+session_start();
+
+require 'controller.php';
+require 'database.php';
+if(isset($_SESSION['login'])){
+    $id_admin = $_SESSION["id_admin"];
+    $nama = $_SESSION["nama"];
+    $username = $_SESSION["username"];
+    $level = $_SESSION["level"];
+}
+?>
