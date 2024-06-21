355bd49906dce48e63b7c308170dc8aefdaf529c
ckckckcz <rriovld@gmail.com>
2024-06-21 07:17:44 +0700

    done : setup data formulir

--- SistemInformasi/form-data.php
+++ SistemInformasi/form-data.php
@@ -7,5 +7,26 @@ if (!isset($_SESSION["login"])){
         alert('Login terlebih dahulu');
         document.location.href = 'login.php';
     </script>";
+
+    exit;
+}
+
+$title = 'Halaman Formulir Pendataan';
+inlcude 'layout/header.php';
+
+if(!isset($_POST['tambah'])){
+    if(create_data($_POST) > 0){
+        echo 
+        "<script>
+            alert('Data Konten Berhasil Ditambahkan');
+            document.location.href = 'form-data.php';
+        </script>";
+    } else {
+        echo
+        "<script>
+            alert('Data Konten Gagal Ditambahkan');
+            document.location.href = 'form-data.php';
+        </script>";
+    }
 }
 ?>
