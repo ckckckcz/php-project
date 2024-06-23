d63ade725516b2eb13a6b1d7819b02d01c78b580
ckckckcz <rriovld@gmail.com>
2024-06-23 18:16:20 +0700

    setup : Route Controller Barang

--- Sistem Penjualan/routes/web.php
+++ Sistem Penjualan/routes/web.php
@@ -27,6 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::get('dashboard', function (){
         return view('dashboard');
     })->name('dashboard');
+
+    Route::controller(BarangController::class)->prefix('barang')->group(function () {
+		Route::get('', 'index')->name('barang');
+		Route::get('tambah', 'tambah')->name('barang.tambah');
+		Route::post('tambah', 'simpan')->name('barang.tambah.simpan');
+		Route::get('edit/{id}', 'edit')->name('barang.edit');
+		Route::post('edit/{id}', 'update')->name('barang.tambah.update');
+		Route::get('hapus/{id}', 'hapus')->name('barang.hapus');
+	});
 });
 
 
